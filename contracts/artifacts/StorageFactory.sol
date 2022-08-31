36d7e1ffb59a5e02c1fd043c4348f4d88d3bdf23
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-08-31 18:04:50 -0400

    added interaction functions and getter functions

--- contracts/artifacts/StorageFactory.sol
+++ contracts/artifacts/StorageFactory.sol
@@ -6,9 +6,19 @@ import "./SimpleStorage.sol";
 contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
 
-function createSimpleStorageContract() public {  // to manage our SimpleStorage contracts
-    SimpleStorage simpleStorage = new SimpleStorage();
-    simpleStorageArray.push(simpleStorage);
-}
+    function createSimpleStorageContract() public {  // to manage our SimpleStorage contracts
+       SimpleStorage simpleStorage = new SimpleStorage();
+       simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        SimpleStorage simpleStorage = SimpleStorage(simpleStorageArray[_simpleStorageIndex]);
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
 
 }
