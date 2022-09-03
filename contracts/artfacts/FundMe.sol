df2367e01e7757935f1dedc3a0cb66e8cdf111a3
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-09-03 16:42:45 -0400

    setting up a funding and withdrawing contract

---
+++ contracts/artfacts/FundMe.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+// get funds from users
+// withdraw funds 
+// set a minimum funding value in USD 
+
+pragma solidity ^0.8.0;
+
+
+contract FundMe {
+      function fund() public payable {
+          // need to be able to set a minimum fund amount in USD 
+          // 1. How do we send ETH to this contract?
+      }
+
+    //   function withdraw(){}
+}
