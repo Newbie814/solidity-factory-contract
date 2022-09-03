beb9ecfca6d1ce01edc99e8eb30ec2c0811b4767
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-09-03 16:59:08 -0400

    revert parameters

--- contracts/artfacts/FundMe.sol
+++ contracts/artfacts/FundMe.sol
@@ -8,9 +8,14 @@ pragma solidity ^0.8.0;
 
 
 contract FundMe {
+      
+      uint256 public minimumUsd = 50;
+
       function fund() public payable {
           // need to be able to set a minimum fund amount in USD 
           // 1. How do we send ETH to this contract?
+        
+          require(msg.value >= minimumUsd, "Minimum funding amount not met");  // 1e18 = 1 * 10 ** 18 == 1000000000000000000
       }
 
     //   function withdraw(){}
