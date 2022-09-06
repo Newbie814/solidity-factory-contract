88987ece00e9cca9a6c5309ece42ec4dfbb3c19e
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-09-06 14:36:19 -0400

    updated contract

--- contracts/artfacts/FundMe.sol
+++ contracts/artfacts/FundMe.sol
@@ -10,13 +10,18 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
       
-      uint256 public minimumUsd = 50;
+      uint256 public minimumUsd = 50 * 1e18;
+
+      address[] public funders;
+      mapping(address => uint256) public addressToAmountFunded;
 
       function fund() public payable {
           // need to be able to set a minimum fund amount in USD 
           // 1. How do we send ETH to this contract?
         
-          require(msg.value >= minimumUsd, "Minimum funding amount not met");  // 1e18 = 1 * 10 ** 18 == 1000000000000000000
+          require(getConversionRate(msg.value) >= minimumUsd, "Minimum funding amount not met");  // 1e18 = 1 * 10 ** 18 == 1000000000000000000
+          funders.push(msg.sender);
+          addressToAmountFunded[msg.sender] = msg.value;
       }
 
       // get ETH price
@@ -29,6 +34,11 @@ contract FundMe {
 
           return uint256(price * 1e10); // 1**10 == 10000000000
 
+      }
+
+      function getVersion() public view returns (uint256) {
+          AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+          return priceFeed.version();
       }
 
 
