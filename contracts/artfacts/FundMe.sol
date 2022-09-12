b1e721c2a31863dacddabb56322df3663c9884b5
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-09-12 19:22:25 -0400

    gas optimizations - var types and custom error handling

--- contracts/artfacts/FundMe.sol
+++ contracts/artfacts/FundMe.sol
@@ -8,52 +8,61 @@ pragma solidity ^0.8.0;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+error MinEthReqFail();
+error CallFail();
+
 contract FundMe {
       
       using PriceConverter for uint256;
       
-      uint256 public minimumUsd = 50 * 1e18;
+      uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
 
       address[] public funders;
       mapping(address => uint256) public addressToAmountFunded;
 
-      address public owner;
+      address public immutable i_owner;
+
+        modifier onlyOwner {
+           //require(msg.sender == i_owner, "Sender is not the owner of this contract");
+           if(msg.sender != i_owner) {revert NotOwner();}
+           _;
+       }
+
 
       constructor () {
-          owner = msg.sender;
+          i_owner = msg.sender;
 
       }
 
       function fund() public payable {
-         
-        
-          require(msg.value.getConversionRate() >= minimumUsd, "Minimum funding amount not met");  // 1e18 = 1 * 10 ** 18 == 1000000000000000000
-          funders.push(msg.sender);
-          addressToAmountFunded[msg.sender] = msg.value;
-      }
+        // require(msg.value.getConversionRate() >= MINIMUM_USD, "Transaction does not meet the minimom ETH requirements.");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        if(msg.value.getConversionRate() < MINIMUM_USD) {revert MinEthReqFail();}
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
 
 
-
+     
 
 
        function withdraw() public onlyOwner {
-           
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        // require(callSuccess, "Call failed");
+        if(!callSuccess) {revert CallFail();}
+    }
 
-           // for( starting index, ending index, step amount )
-           for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex++ ){
-               address funder = funders[funderIndex];
-               addressToAmountFunded[funder] = 0;
-
-           }
-           //reset array
-           funders = new address[](0);      
-               (bool callSuccess, /*bytes memory dataReturned*/) = payable(msg.sender).call{value: address(this).balance}(""); 
-               require(callSuccess, "Call failed");
-
-       }
-
-       modifier onlyOwner {
-           require(msg.sender == owner, "Sender is not the owner of this contract");
-           _;
-       }
-}
+      
+}    
